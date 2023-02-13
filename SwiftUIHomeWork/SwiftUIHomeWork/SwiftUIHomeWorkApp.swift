debd0d5fcbc1ff6fbf4398753b3eae7927710535
Vasili13 <90265333+Vasili13@users.noreply.github.com>
2023-02-13 16:36:12 +0300

    First commit

---
+++ SwiftUIHomeWork/SwiftUIHomeWork/SwiftUIHomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHomeWorkApp.swift
+//  SwiftUIHomeWork
+//
+//  Created by Василий Вырвич on 13.02.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
